fa2a75bf24ad443da7cbca74aefbd8b691d9f5fc
Nguyễn Minh Khoa <nguyenminhkhoa@Nguyens-MacBook-Pro.local>
2019-09-12 19:52:24 -0700

    I'm sorry, I know it's late but I just figure out the right way to do the patentInfo.js

---
+++ patentInfo.js
@@ -0,0 +1,232 @@
+var patInfo = [
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  },
+  {
+    "patNum": "9,450,865",
+    "title": "Open shortest path first extensions in support of wavelength switched optical networks, ",
+    "date": "September 20, 2016"
+  }
+]
