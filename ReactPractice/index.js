243226b5bad5b05225a5709e07c6572ff7598bbc
KhoaNguyen <nguyenminhkhoa@Nguyens-MacBook-Pro.local>
2019-09-26 12:12:36 -0700

    update 5a

--- ReactPractice/index.js
+++ ReactPractice/index.js
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom";
 import chemElements from "./elements.json";
 
 // What is this? HTML mixed with JavaScript
-let head1 = <h1>The Periodic Table</h1>;
-let head2 = <h2>Brought to you by Khoa Nguyen sq9943</h2>;
-let intro = <p>There are 118 chemical elements.</p>;
 
-ReactDOM.render(<section>
-    {head1}
-    {head2}
-    {intro}
-    </section>,
+let contents = <section>
+		<h1>The Periodic Table</h1>
+		<h2>Brought to you by Khoa Nguyen sq9943</h2>
+		<p>There are {parseInt(chemElements.length)} chemical elements.</p>
+		</section>;
+
+ReactDOM.render(contents,
     document.getElementById("root")
 );
---
+++ ReactPractice/periodic.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import chemElements from "./elements.json";
+
+// What is this? HTML mixed with JavaScript
+let head1 = <h1>The Periodic Table</h1>;
+let head2 = <h2>Brought to you by Khoa Nguyen sq9943</h2>;
+let intro = <p>There are 118 chemical elements.</p>;
+
+ReactDOM.render(<section>
+    {head1}
+    {head2}
+    {intro}
+    </section>,
+    document.getElementById("root")
+);
